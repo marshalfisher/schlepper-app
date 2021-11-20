be79b9a420f4a194e2e924b428bf998021ddcda3
Pedro Ramos <pedrotr@hotmail.com>
2021-11-20 17:08:18 -0300

    slices all done

--- schlepper/src/redux/store.ts
+++ schlepper/src/redux/store.ts
@@ -1,13 +1,13 @@
 import { createStore, configureStore } from '@reduxjs/toolkit';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-// import rootReducer from './slices';
-import reducers from './reducers';
+import rootReducer from './slices';
+// import reducers from './reducers';
 
 const persistConfig = {
   key: 'root',
   storage,
 };
 
-const persistedReducer = persistReducer(persistConfig, reducers);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 export const store = createStore(persistedReducer);
